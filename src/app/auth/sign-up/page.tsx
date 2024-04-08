a27e36ee79d6f03208cda6d9a00bee30befb3f5a
rdx <reydux95@gmail.com>
2024-04-09 00:28:59 +0200

    fix(sign-up): change page description

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -7,10 +7,7 @@ const Page = () => {
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
-            <h1 className="text-3xl font-bold">Login</h1>
-            <p className="text-balance text-muted-foreground">
-              Enter your email below to login to your account
-            </p>
+            <h1 className="text-3xl font-bold">Sign Up</h1>
           </div>
           <SignUpForm />
         </div>
