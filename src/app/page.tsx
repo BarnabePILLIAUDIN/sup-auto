1eb9e6dd211e3363a4424f677d7dcc46f482c1cd
Barnabé PILLIAUDIN <barnabe.pilliaudin@gmail.com>
2024-04-08 10:52:43 +0200

    fix:Made site worked again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import Link from "next/link";
 
-import { CreatePost } from "@/app/_components/create-post";
 import { api } from "@/trpc/server";
 
-export default async function Home() {
+const Home = async () => {
   const hello = await api.post.hello({ text: "from tRPC" });
 
   return (
@@ -41,25 +40,9 @@ export default async function Home() {
             {hello ? hello.greeting : "Loading tRPC query..."}
           </p>
         </div>
-
-        <CrudShowcase />
       </div>
     </main>
   );
-}
+};
 
-async function CrudShowcase() {
-  const latestPost = await api.post.getLatest();
-
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
-    </div>
-  );
-}
+export default Home;
