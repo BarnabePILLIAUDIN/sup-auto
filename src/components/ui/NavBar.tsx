3fe44294fcc853c7ffda4808ac451ce398c7e9f3
Barnabé PILLIAUDIN <barnabe.pilliaudin@gmail.com>
2024-04-09 10:26:55 +0200

    feat: Not working client side perm

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -1,22 +1,38 @@
+"use client"
+import useSession from "@/hooks/useSession"
 import Link from "next/link"
 
-const Navbar = () => (
-  <nav className="flex justify-between px-5 py-5">
-    <h1 className="text-2xl font-bold">
-      <Link href="/">SUPOTOMOBILE</Link>
-    </h1>
-    <ul className="flex flex-row gap-5 font-semibold">
-      <li>
-        <Link href="/cars">Cars</Link>
-      </li>
-      <li>
-        <Link href="/auth/sign-in">Sign in</Link>
-      </li>
-      <li>
-        <Link href="/auth/sign-up">Sign up</Link>
-      </li>
-    </ul>
-  </nav>
-)
+const Navbar = () => {
+  const { session, signOut } = useSession()
+
+  return (
+    <nav className="flex justify-between px-5 py-5">
+      <h1 className="text-2xl font-bold">
+        <Link href="/">SUPOTOMOBILE</Link>
+      </h1>
+      <ul className="flex flex-row gap-5 font-semibold">
+        <li>
+          <Link href="/cars">Cars</Link>
+        </li>
+        {session ? (
+          <>
+            <li>
+              <Link href="/auth/sign-in">Sign in</Link>
+            </li>
+            <li>
+              <Link href="/auth/sign-up">Sign up</Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <button onClick={signOut}>Sign out</button>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  )
+}
 
 export default Navbar
